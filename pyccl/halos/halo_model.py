fb2d54ec92fe00cf7294ef5c0560bb4838fec260
Lucas James Faga <ljfaga@MacBook-Air-de-Lucas.local>
2023-04-19 13:23:18 -0400

    Remove a temporary modification made for a test in halomod_trispectrum_4h

--- pyccl/halos/halo_model.py
+++ pyccl/halos/halo_model.py
@@ -1742,9 +1742,8 @@ def halomod_trispectrum_4h(cosmo, hmc, k, a, prof1, prof2=None,
         out = np.squeeze(out, axis=0)
     if np.ndim(k) == 0:
         out = np.squeeze(out, axis=-1)
-        out = np.squeeze(out, axis=-1)
-    # LJF 
-    return t1113 + t1122 # out
+        out = np.squeeze(out, axis=-1) 
+    return out
 
 def halomod_Tk3D_1h(cosmo, hmc,
                     prof1, prof2=None, prof12_2pt=None,
